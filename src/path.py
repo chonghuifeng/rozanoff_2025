7ab19991320c24f2fba646e9cd23b5312b4737bc
chf21 <chf21@ic.ac.uk>
2025-11-13 10:58:27 +0100

    flame_length_gpr 13 nov

---
+++ src/path.py
@@ -0,0 +1,21 @@
+"""
+Module to define the paths used in the code
+"""
+
+###########
+# Imports #
+###########
+
+import os
+
+#########
+# Paths #
+#########
+
+# Root
+PATH_ROOT_FOLDER = os.path.dirname(os.path.dirname(__file__))
+PATH_DATA_FOLDER = os.path.join(PATH_ROOT_FOLDER, "original_data")
+
+# Data
+PATH_DATASETV1 = os.path.join(PATH_DATA_FOLDER, "DataSetV1.xlsx")
+PATH_DATA_FLAMELENGTH = os.path.join(PATH_DATA_FOLDER, "FlameLength.xlsx")
