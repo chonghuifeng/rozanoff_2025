664fa39c0b4b53fc1cb18505ace95245d02fc370
chf21 <chf21@ic.ac.uk>
2025-12-15 23:24:49 +0100

    implementation of different kernels and SHAP analysis

--- src/path.py
+++ src/path.py
@@ -17,6 +17,7 @@ import os
 # Root
 PATH_ROOT_FOLDER = os.path.dirname(os.path.dirname(__file__))
 PATH_DATA_FOLDER = os.path.join(PATH_ROOT_FOLDER, "original_data")
+PATH_FLAMELENGTH_FOLDER = os.path.join(PATH_ROOT_FOLDER, "flame_length_pred")
 
 # Data
 PATH_DATASETV1 = os.path.join(PATH_DATA_FOLDER, "DataSetV1.xlsx")
