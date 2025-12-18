ad30086f04975ac605b0a5dc443d41b04b017270
chf21 <chf21@ic.ac.uk>
2025-12-18 12:38:48 +0100

    final model 18 dec

--- src/path.py
+++ src/path.py
@@ -22,4 +22,6 @@ PATH_FLAMELENGTH_FOLDER = os.path.join(PATH_ROOT_FOLDER, "flame_length_pred")
 # Data
 PATH_DATASETV1 = os.path.join(PATH_DATA_FOLDER, "DataSetV1.xlsx")
 PATH_DATA_FLAMELENGTH = os.path.join(PATH_DATA_FOLDER, "FlameLength.xlsx")
-PATH_DATA_FLAMELENGTH_NEW = os.path.join(PATH_DATA_FOLDER, "DatasetFlameComplet.xlsx")
+PATH_DATA_FLAMELENGTH_NEW = os.path.join(
+    PATH_FLAMELENGTH_FOLDER, "DatasetFlameComplet.xlsx"
+)
